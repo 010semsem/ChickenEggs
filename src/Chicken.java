48c15404131e28b89a70d19e1a789e981ea9f82f
اسمك <xabdovx@gmail.com>
2025-12-09 01:17:07 +0200

    first commit

---
+++ src/Chicken.java
@@ -0,0 +1,155 @@
+//import AnimListener;
+import Texture.TextureReader;
+import java.awt.event.*;
+import java.io.IOException;
+import javax.media.opengl.*;
+
+import java.util.BitSet;
+import javax.media.opengl.glu.GLU;
+
+public class Chicken extends AnimListener {
+
+
+    int maxWidth = 100;
+    int maxHeight = 100;
+    int xPlayer = maxWidth/2, yPlayer = maxHeight/2;
+
+    String textureNames[] = {"","","","","Back.png"};
+    TextureReader.Texture texture[] = new TextureReader.Texture[textureNames.length];
+    int textures[] = new int[textureNames.length];
+
+    public void init(GLAutoDrawable gld) {
+
+        GL gl = gld.getGL();
+        gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);    //This Will Clear The Background Color To Black
+
+        gl.glEnable(GL.GL_TEXTURE_2D);  // Enable Texture Mapping
+        gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
+        gl.glGenTextures(textureNames.length, textures, 0);
+
+        for(int i = 0; i < textureNames.length; i++){
+            try {
+                texture[i] = TextureReader.readTexture(assetsFolderName + "//" + textureNames[i] , true);
+                gl.glBindTexture(GL.GL_TEXTURE_2D, textures[i]);
+
+
+                new GLU().gluBuild2DMipmaps(
+                        GL.GL_TEXTURE_2D,
+                        GL.GL_RGBA,
+                        texture[i].getWidth(), texture[i].getHeight(),
+                        GL.GL_RGBA,
+                        GL.GL_UNSIGNED_BYTE,
+                        texture[i].getPixels()
+                );
+            } catch( IOException e ) {
+                System.out.println(e);
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void display(GLAutoDrawable gld) {
+
+        GL gl = gld.getGL();
+        gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+        gl.glLoadIdentity();
+
+        DrawBackground(gl);
+        handleKeyPress();
+
+        //DrawSprite(gl, ((xPlayer/(maxWidth/2))-0.9), (yPlayer/(maxHeight/2)-1.7), 0, 1);
+        DrawSprite(gl, xPlayer,yPlayer, 0, 1);
+    }
+
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+    }
+
+    public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
+    }
+
+    public void DrawSprite(GL gl,double x, double y, int index, float scale){
+        gl.glEnable(GL.GL_BLEND);
+        gl.glBindTexture(GL.GL_TEXTURE_2D, textures[index]);
+        gl.glPushMatrix();
+       gl.glTranslated(x/(maxWidth/2)-1, y/(maxHeight/2.0) - 1.6, 0);
+       // gl.glTranslated( x , y , 0);
+        gl.glScaled(0.1*scale, 0.1*scale, 1);
+
+        gl.glBegin(GL.GL_QUADS);
+        gl.glTexCoord2f(0.0f, 0.0f);
+        gl.glVertex3f(-1.0f, -1.0f, -1.0f);
+        gl.glTexCoord2f(1.0f, 0.0f);
+        gl.glVertex3f(1.0f, -1.0f, -1.0f);
+        gl.glTexCoord2f(1.0f, 1.0f);
+        gl.glVertex3f(1.0f, 1.0f, -1.0f);
+        gl.glTexCoord2f(0.0f, 1.0f);
+        gl.glVertex3f(-1.0f, 1.0f, -1.0f);
+        gl.glEnd();
+        gl.glPopMatrix();
+
+        gl.glDisable(GL.GL_BLEND);
+    }
+
+    public void DrawBackground(GL gl){
+        gl.glEnable(GL.GL_BLEND);
+        gl.glBindTexture(GL.GL_TEXTURE_2D, textures[textureNames.length-1]);
+
+        gl.glPushMatrix();
+        gl.glBegin(GL.GL_QUADS);
+        // Front Face
+        gl.glTexCoord2f(0.0f, 0.0f);
+        gl.glVertex3f(-1.0f, -1.0f, -1.0f);
+        gl.glTexCoord2f(1.0f, 0.0f);
+        gl.glVertex3f(1.0f, -1.0f, -1.0f);
+        gl.glTexCoord2f(1.0f, 1.0f);
+        gl.glVertex3f(1.0f, 1.0f, -1.0f);
+        gl.glTexCoord2f(0.0f, 1.0f);
+        gl.glVertex3f(-1.0f, 1.0f, -1.0f);
+        gl.glEnd();
+        gl.glPopMatrix();
+
+        gl.glDisable(GL.GL_BLEND);
+    }
+
+    /*
+     * KeyListener
+     */
+
+    public void handleKeyPress() {
+
+        if (isKeyPressed(KeyEvent.VK_LEFT)) {
+            if (xPlayer > 5) {
+                xPlayer--;
+            }
+        }
+        if (isKeyPressed(KeyEvent.VK_RIGHT)) {
+            if (xPlayer < maxWidth-5) {
+                xPlayer++;
+            }
+
+        }
+    }
+
+    public BitSet keyBits = new BitSet(256);
+
+    @Override
+    public void keyPressed(final KeyEvent event) {
+        int keyCode = event.getKeyCode();
+        keyBits.set(keyCode);
+    }
+
+    @Override
+    public void keyReleased(final KeyEvent event) {
+        int keyCode = event.getKeyCode();
+        keyBits.clear(keyCode);
+    }
+
+    @Override
+    public void keyTyped(final KeyEvent event) {
+        // don't care 
+    }
+
+    public boolean isKeyPressed(final int keyCode) {
+        return keyBits.get(keyCode);
+    }
+}
---
+++ src/test.java
@@ -0,0 +1,35 @@
+import com.sun.opengl.util.Animator;
+import com.sun.opengl.util.FPSAnimator;
+import javax.media.opengl.GLCanvas;
+import javax.swing.*;
+import java.awt.*;
+
+public class test extends JFrame {
+
+    public static void main(String[] args) {
+        new test();
+    }
+
+
+    public test() {
+        GLCanvas glcanvas;
+        Animator animator;
+
+        AnimListener listener = new Chicken();
+        glcanvas = new GLCanvas();
+        glcanvas.addGLEventListener(listener);
+        glcanvas.addKeyListener(listener);
+        getContentPane().add(glcanvas, BorderLayout.CENTER);
+        animator = new FPSAnimator(15);
+        animator.add(glcanvas);
+        animator.start();
+
+        setTitle("solider Test");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(700, 700);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setFocusable(true);
+        glcanvas.requestFocus();
+    }
+}
